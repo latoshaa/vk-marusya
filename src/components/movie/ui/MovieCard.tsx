3519e8c1f82e671687de446d9b935ab9292051aa
latoshaa <latonova2308@gmail.com>
2025-10-22 08:43:26 +0300

    add main page

---
+++ src/components/movie/ui/MovieCard.tsx
@@ -0,0 +1,39 @@
+import { FC, useCallback } from "react";
+import { Movie } from "../model/types";
+import styles from "./MovieCard.module.scss";
+
+interface MovieCardProps {
+  movie: Movie;
+  number: number;
+  onClick: (movieId: number) => void;
+}
+
+export const MovieCard: FC<MovieCardProps> = ({ movie, number, onClick }) => {
+  const handleClick = useCallback(() => {
+    onClick(movie.id);
+  }, [movie.id, onClick]);
+
+  const isDoubleDigit = number >= 10;
+
+  return (
+    <div className={styles.movieCard} onClick={handleClick}>
+      <div
+        className={`${styles.numberBadge} ${
+          isDoubleDigit ? styles.doubleDigit : ""
+        }`}
+      >
+        <span className={styles.number}>{number}</span>
+      </div>
+
+      <div className={styles.imageWrapper}>
+        <img src={movie.posterUrl} alt={movie.title} className={styles.image} />
+
+        <div className={styles.overlay}>
+          <div className={styles.content}>
+            <h3 className={styles.title}>{movie.title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
