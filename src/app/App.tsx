c0bd00c683ac4cd7ed8c1c9a3f66ddb85e041361
latoshaa <latonova2308@gmail.com>
2025-11-05 14:20:02 +0300

    add suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Suspense } from 'react';
 import { Header } from '@widgets/header';
 import { Footer } from "@widgets/footer";
 import { HomePage } from '@pages/home';
 import { GenresPage } from '@pages/genres';
 import { ErrorBoundary } from '@shared/ui/ErrorBoundary';
+import { LoadingSpinner } from '@shared/ui/LoadingSpinner';
 import '@shared/styles/global.scss';
 
 function App() {
@@ -14,12 +16,14 @@ function App() {
           <Header />
           <main className="main-container">
             <div className="page-container">
+              <Suspense fallback={<LoadingSpinner />}>
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/genres" element={<GenresPage />} />
                 <Route path="/genres/:id" element={<div>Страница жанра</div>} />
                 <Route path="/movie/:id" element={<div>Страница фильма</div>} />
               </Routes>
+              </Suspense>
             </div>
           </main>
           <Footer />
